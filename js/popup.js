6d7bc81cba5247cbc79720cdec4ff9798b9c1995
Igor Shangin <igorsan1@mail.ru>
2020-01-06 17:23:46 +0300

    Implement data transfer to template editor

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,46 @@
 Vue.component('template-item', {
-    props: ['tmplt'],
-    template: '<li class="list-group-item">' + 
+    props: ['snippet', 'template'],
+    template: '<li class="list-group-item" title="Click to edit" v-on:click="editTemplate">' + 
                     '<span class="badge badge-primary rounded-pill mr-1">' + 
-                        '{{tmplt[0]}}' + 
+                        '{{snippet}}' + 
                     '</span>' + 
-                    '{{tmplt[1]}}' + 
-                    '<button class="btn btn-sm btn-edit btn-light float-right"></button>' + 
+                    '{{template}}' + 
             '</li>',
+    methods:  {
+        editTemplate: function(){
+            this.$root.$emit('edit-template', this.snippet, this.template);
+        }
+    }
+})
+
+Vue.component('template-editor', {
+    data: function(){
+        return {
+            prevSnippet : null,
+            snippet : null, 
+            template : null
+        }
+    },
+
+    mounted: function() {
+        var editor = this;
+        this.$root.$on('edit-template', function (snippet, template) {
+            editor.prevSnippet = snippet;
+            editor.snippet = snippet;
+            editor.template = template;
+        });
+    },
+    
+    methods: {
+        onSubmit: function(event) {
+            alert("wrong");
+            event.preventDefault();
+        }
+    },
+    template: '<form id="editor" v-on:submit="onSubmit">' + 
+                    '<input v-model="snippet" type="text" class="form-control" placeholder="New snippet">' + 
+                    '<textarea v-model="template" class="form-control" placeholder="New template"></textarea>' + 
+                '</form>',
 })
 
 new Vue({
