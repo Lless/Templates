1b408c4933f168954897f3015812e2fe737506ae
Igor Shangin <igorsan1@mail.ru>
2020-01-15 01:56:12 +0300

    extension replases snippets from storage now. Extension works

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,10 +1,14 @@
-function getTemplate(snippet) {
-    if (snippet == "such") return "wow";
-    return null;
+async function getTemplate(snippet) {
+    let promise = new Promise(resolve => {
+        chrome.storage.sync.get(snippet, res => {
+            resolve(res[snippet]);
+        });
+    });
+    return await promise;
 }
 
 //returns true if snippet replaced, false otherwise
-function replaceContentEditable() {
+async function replaceContentEditable() {
     var node = document.getSelection().focusNode;
     if (!node || !node.parentElement.isContentEditable) return false;
     
@@ -15,9 +19,10 @@ function replaceContentEditable() {
     var trueWordStart = Math.max(wordRange.toString().lastIndexOf(" "), 
                                 wordRange.toString().lastIndexOf("\n")) + 1;
     wordRange.setStart(node, trueWordStart);
-    console.log(wordRange.toString());
+    console.log("found snippet: " + wordRange.toString());
 
-    var template = getTemplate(wordRange.toString());
+    var template = await getTemplate(wordRange.toString());
+    console.log("found template: " + template);
     if (!template) return false;
     wordRange.deleteContents();
     wordRange.insertNode(document.createTextNode(template));
@@ -26,27 +31,26 @@ function replaceContentEditable() {
 }    
 
 //returns true if snippet replaced, false otherwise
-function replaceInputAndTextarea(input) {
-    if(!(input.value) || !(input.selectionEnd) || input.readOnly || input.disabled) {
-        console.log("not input");
+async function replaceInputAndTextarea(input) {
+    if(!(input.value) || !(input.selectionEnd) || input.readOnly || input.disabled) 
         return false;
-    }
 
     var word = input.value.substring(0, input.selectionEnd)
     var wordStart = Math.max(word.lastIndexOf(" "), word.lastIndexOf("\n")) + 1;
     word = word.substring(wordStart);
-    var template = getTemplate(word);
-    console.log(word);
+    console.log("found snippet: " + word);
 
+    var template = await getTemplate(word);
+    console.log("found template: " + template);
     if (!template) return false;
     input.value = input.value.substring(0, wordStart) 
-                + getTemplate(word) 
+                + template
                 + input.value.substring(input.selectionEnd);
     return true;
 }
 
-Mousetrap.bindGlobal("tab", event => {
-    if (replaceContentEditable() || replaceInputAndTextarea(event.target)) {
+Mousetrap.bindGlobal("tab", async event => {
+    if ((await replaceContentEditable()) || (await replaceInputAndTextarea(event.target))) {
         event.preventDefault();
         event.stopPropagation();
     }
